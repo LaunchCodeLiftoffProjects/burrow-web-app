222bb65a9b219691d1b37543f655b071bf882895
WillPohlman <william.a.pohlman@gmail.com>
2020-12-05 14:39:01 -0600

    Created PropertyRepository and RoomRepository. Property class should now be usable for the views and controllers.

--- src/main/java/com/example/burrowwebapp/models/Property.java
+++ src/main/java/com/example/burrowwebapp/models/Property.java
@@ -1,19 +1,52 @@
 package com.example.burrowwebapp.models;
 
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 public class Property extends AbstractEntity {
 
+    @OneToMany(mappedBy = "property")
     private final List<Room> rooms = new ArrayList<>();
 
-    public Property(@NotBlank String name){
+    @NotBlank
+    private String location;
+
+    @NotBlank
+    @Size(min = 1, max = 250, message = "Description must be between 1 and 250 characters")
+    private String description;
+
+    public Property(@NotBlank String name, @NotBlank String location,
+                    @NotBlank @Size(min = 1, max = 250, message = "Description must be between 1 and 250 characters") String description)
+    {
         this.setName(name);
+        this.location = location;
+        this.description = description;
     }
 
     public Property(){}
 
+    public String getLocation()
+    {
+        return location;
+    }
+
+    public void setLocation(String location)
+    {
+        this.location = location;
+    }
+
+    public String getDescription()
+    {
+        return description;
+    }
+
+    public void setDescription(String description)
+    {
+        this.description = description;
+    }
 }
--- src/main/java/com/example/burrowwebapp/models/Room.java
+++ src/main/java/com/example/burrowwebapp/models/Room.java
@@ -1,10 +1,16 @@
 package com.example.burrowwebapp.models;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
 
 @Entity
 public class Room extends AbstractEntity
 {
+    @ManyToOne
+    @NotNull
+    private Property property;
+
     public Room(){
 
     }
