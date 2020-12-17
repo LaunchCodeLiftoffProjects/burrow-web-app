623b52b0d3d6f42bb826f9d075af1bcef282dad9
Liza Baez Kemp <baezkemp@gmail.com>
2020-12-16 21:20:50 -0600

    Fix device repository

--- src/main/java/com/example/burrowwebapp/data/DeviceRepository.java
+++ src/main/java/com/example/burrowwebapp/data/DeviceRepository.java
@@ -2,6 +2,8 @@ package com.example.burrowwebapp.data;
 
 import com.example.burrowwebapp.models.Device;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DeviceRepository extends CrudRepository<Device, Integer> {
 }
