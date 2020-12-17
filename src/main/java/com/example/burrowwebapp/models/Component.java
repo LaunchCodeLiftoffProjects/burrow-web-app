ca3525f65a315bc08d05285f0e9091228f9955ee
John Wasielewicz <wazjohn@yahoo.com>
2020-12-17 10:47:52 -0600

    update component model

--- src/main/java/com/example/burrowwebapp/models/Component.java
+++ src/main/java/com/example/burrowwebapp/models/Component.java
@@ -1,7 +1,40 @@
 package com.example.burrowwebapp.models;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Component extends AbstractEntity {
+
+    @Size(max = 250, message = "Description too long!")
+    private String description;
+
+    @ManyToMany(mappedBy = "components")
+    private List<Device> devices = new ArrayList<>();
+
+    public Component(String name, @Size(max = 250, message = "Description too long!") String description) {
+        this.setName(name);
+        this.description = description;
+    }
+
+    public Component() {}
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Device> getDevices() {
+        return devices;
+    }
+
+    public void setDevices(List<Device> devices) {
+        this.devices = devices;
+    }
 }
