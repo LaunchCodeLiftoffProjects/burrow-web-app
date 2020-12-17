fb36662a2c75535d1cb7be0db14c4490eb17d9f7
John Wasielewicz <wazjohn@yahoo.com>
2020-12-17 11:55:59 -0600

    fix component model and device model

--- src/main/java/com/example/burrowwebapp/models/Component.java
+++ src/main/java/com/example/burrowwebapp/models/Component.java
@@ -1,7 +1,7 @@
 package com.example.burrowwebapp.models;
 
 import javax.persistence.Entity;
-import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
 import javax.validation.constraints.Size;
 import java.util.ArrayList;
 import java.util.List;
@@ -12,8 +12,8 @@ public class Component extends AbstractEntity {
     @Size(max = 250, message = "Description too long!")
     private String description;
 
-    @ManyToMany(mappedBy = "components")
-    private final List<Device> devices = new ArrayList<>();
+    @ManyToOne
+    private Device device;
 
     public Component(String name, @Size(max = 250, message = "Description too long!") String description) {
         this.setName(name);
@@ -30,7 +30,11 @@ public class Component extends AbstractEntity {
         this.description = description;
     }
 
-    public List<Device> getDevices() {
-        return devices;
+    public Device getDevice() {
+        return device;
+    }
+
+    public void setDevice(Device device) {
+        this.device = device;
     }
 }
