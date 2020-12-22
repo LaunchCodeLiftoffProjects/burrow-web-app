53c008513cb345749cbac63c22a990b9d9578109
Sarah <abantaos@hotmail.com>
2020-12-21 21:03:14 -0600

    authentication filter HandlerInterceptorAdapter and Autowired

--- src/main/java/com/example/burrowwebapp/AuthenticationFilter.java
+++ src/main/java/com/example/burrowwebapp/AuthenticationFilter.java
@@ -1,4 +1,16 @@
 package com.example.burrowwebapp;
 
-public class AuthenticationFilter {
+import com.example.burrowwebapp.controller.AuthenticationController;
+import com.example.burrowwebapp.data.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class AuthenticationFilter extends HandlerInterceptorAdapter {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    AuthenticationController authenticationController;
+
 }
