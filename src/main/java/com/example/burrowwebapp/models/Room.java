5b4d77cd7e4a26dc6978431acab8c82dba9afffe
John Wasielewicz <wazjohn@yahoo.com>
2020-12-22 15:19:01 -0600

    delete room will delete related devices without error

--- src/main/java/com/example/burrowwebapp/models/Room.java
+++ src/main/java/com/example/burrowwebapp/models/Room.java
@@ -1,9 +1,6 @@
 package com.example.burrowwebapp.models;
 
-import javax.persistence.Entity;
-import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
@@ -15,7 +12,7 @@ public class Room extends AbstractEntity {
     @ManyToOne
     private Property property;
 
-    @OneToMany (mappedBy = "room")
+    @OneToMany (mappedBy = "room", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Device> devices = new ArrayList<>();
 
     public Room(){ }
