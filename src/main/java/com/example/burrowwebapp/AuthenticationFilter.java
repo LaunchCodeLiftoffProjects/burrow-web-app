60496a2856ee2049219e98ac308df17ec30dedec
Sarah <abantaos@hotmail.com>
2020-12-21 21:06:58 -0600

    started prehandle

--- src/main/java/com/example/burrowwebapp/AuthenticationFilter.java
+++ src/main/java/com/example/burrowwebapp/AuthenticationFilter.java
@@ -2,8 +2,14 @@ package com.example.burrowwebapp;
 
 import com.example.burrowwebapp.controller.AuthenticationController;
 import com.example.burrowwebapp.data.UserRepository;
+import com.example.burrowwebapp.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
 
 public class AuthenticationFilter extends HandlerInterceptorAdapter {
 
@@ -12,5 +18,29 @@ public class AuthenticationFilter extends HandlerInterceptorAdapter {
 
     @Autowired
     AuthenticationController authenticationController;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request,
+                             HttpServletResponse response,
+                             Object handler) throws IOException {
+
+        // Don't require sign-in for whitelisted pages
+        if (isWhitelisted(request.getRequestURI())) {
+            // returning true indicates that the request may proceed
+            return true;
+        }
+
+        HttpSession session = request.getSession();
+        User user = authenticationController.getUserFromSession(session);
+
+        // The user is logged in
+        if (user != null) {
+            return true;
+        }
+
+        // The user is NOT logged in
+        response.sendRedirect("/login");
+        return false;
+    }
 
 }
