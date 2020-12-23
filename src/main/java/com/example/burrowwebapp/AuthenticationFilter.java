ca789310876869dded7619f9b901a43d3c83c963
Sarah <abantaos@hotmail.com>
2020-12-23 11:20:32 -0600

    declared whitelist

--- src/main/java/com/example/burrowwebapp/AuthenticationFilter.java
+++ src/main/java/com/example/burrowwebapp/AuthenticationFilter.java
@@ -10,6 +10,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 public class AuthenticationFilter extends HandlerInterceptorAdapter {
 
@@ -18,6 +20,8 @@ public class AuthenticationFilter extends HandlerInterceptorAdapter {
 
     @Autowired
     AuthenticationController authenticationController;
+
+    private static final List<String> whitelist = Arrays.asList("/login", "/register", "/logout", "/css");
 
     @Override
     public boolean preHandle(HttpServletRequest request,
@@ -40,6 +44,15 @@ public class AuthenticationFilter extends HandlerInterceptorAdapter {
 
         // The user is NOT logged in
         response.sendRedirect("/login");
+        return false;
+    }
+
+    private static boolean isWhitelisted(String path) {
+        for (String pathRoot : whitelist) {
+            if (path.startsWith(pathRoot)) {
+                return true;
+            }
+        }
         return false;
     }
 
