df5ae2ac254e9cd50ee841b659a8cf9b86c5f7d1
John Wasielewicz <wazjohn@yahoo.com>
2020-12-28 11:51:53 -0600

    delete property option in properties view

--- build/resources/main/static/js/burrow.js
+++ build/resources/main/static/js/burrow.js
@@ -3,3 +3,9 @@ function deleteRoom() {
         document.getElementById("delete-room").submit();
     }
 }
+
+unction deleteProperty() {
+    if (confirm("Are you sure?")) {
+        document.getElementById("delete-property").submit();
+    }
+}
