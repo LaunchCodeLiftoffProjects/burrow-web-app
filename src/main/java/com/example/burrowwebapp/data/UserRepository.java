fb20c13b709324bf2aaf4e1f2748db61cc08d1fb
Kaitlyn Forks <67718565+kforks@users.noreply.github.com>
2020-12-28 19:22:41 -0600

    prepping for DTOs

--- src/main/java/com/example/burrowwebapp/data/UserRepository.java
+++ src/main/java/com/example/burrowwebapp/data/UserRepository.java
@@ -1,9 +1,16 @@
 package com.example.burrowwebapp.data;
 
-import com.example.burrowwebapp.models.Device;
+import com.example.burrowwebapp.models.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
-public interface DeviceRepository extends CrudRepository<Device, Integer> {
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+    User findByUsername(String username);
+
 }
+
+
+
--- src/main/java/com/example/burrowwebapp/models/User.java
+++ src/main/java/com/example/burrowwebapp/models/User.java
@@ -1,2 +1,30 @@
-package com.example.burrowwebapp.models;public class User {
+package com.example.burrowwebapp.models;
+
+import com.sun.istack.NotNull;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.persistence.Entity;
+
+@Entity
+public class User {
+
+    private static final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+    @NotNull
+    private String username;
+
+    @NotNull
+    private String pwHash;
+
+    public User() {}
+
+    public User(String username, String password) {
+        this.username = username;
+        this.pwHash = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
 }
