0e39a6d18ffa917101817c3db0d19203a61144e6
Kaitlyn Forks <67718565+kforks@users.noreply.github.com>
2020-12-28 19:46:40 -0600

    DTOs

---
+++ src/main/java/com/example/burrowwebapp/models/RegisterFormDTO.java
@@ -0,0 +1,15 @@
+package com.example.burrowwebapp.models;
+
+public class RegisterFormDTO extends LoginFormDTO {
+
+    private String verifyPassword;
+
+    public String getVerifyPassword() {
+        return verifyPassword;
+    }
+
+    public void setVerifyPassword(String verifyPassword) {
+        this.verifyPassword = verifyPassword;
+    }
+
+}
