04bd4a30da55f035c07d667e6b7d9b261e69ef4a
Kaitlyn Forks <67718565+kforks@users.noreply.github.com>
2021-01-06 20:59:01 -0600

    search redesign

--- src/main/java/com/example/burrowwebapp/controller/SearchController.java
+++ src/main/java/com/example/burrowwebapp/controller/SearchController.java
@@ -1,2 +1,25 @@
-package com.example.burrowwebapp.controller;public class SearchController {
+
+package com.example.burrowwebapp.controller;
+
+import com.example.burrowwebapp.data.DeviceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("search")
+public class SearchController {
+
+    @Autowired
+    private DeviceRepository deviceRepository;
+
+    @RequestMapping("")
+    public String search(Model model) {
+
+
+        return "search";
+    }
+
+
 }
