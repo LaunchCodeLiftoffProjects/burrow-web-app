3231979a3a9c9345aa2cf9bfaf9336ab00cf91c6
John Wasielewicz <wazjohn@yahoo.com>
2021-01-15 16:51:30 -0600

    render registration form within authentication controller

--- src/main/java/com/example/burrowwebapp/controller/AuthenticationController.java
+++ src/main/java/com/example/burrowwebapp/controller/AuthenticationController.java
@@ -2,8 +2,11 @@ package com.example.burrowwebapp.controller;
 
 import com.example.burrowwebapp.data.UserRepository;
 import com.example.burrowwebapp.models.User;
+import com.example.burrowwebapp.models.dto.RegisterFormDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpSession;
 import java.util.Optional;
@@ -33,5 +36,12 @@ public class AuthenticationController {
 
     private static void setUserInSession(HttpSession session, User user) {
         session.setAttribute(userSessionKey, user.getId());
+    }
+
+    @GetMapping("/register")
+    public String displayRegistrationForm(Model model) {
+        model.addAttribute(new RegisterFormDTO());
+        model.addAttribute("title", "Register");
+        return "register";
     }
 }
