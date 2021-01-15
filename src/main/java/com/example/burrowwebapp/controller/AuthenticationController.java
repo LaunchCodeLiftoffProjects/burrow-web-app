5f3927c7c822da8d384d70244909f0cb6b489be2
John Wasielewicz <wazjohn@yahoo.com>
2021-01-15 16:41:32 -0600

    create authentication controller

---
+++ src/main/java/com/example/burrowwebapp/controller/AuthenticationController.java
@@ -0,0 +1,37 @@
+package com.example.burrowwebapp.controller;
+
+import com.example.burrowwebapp.data.UserRepository;
+import com.example.burrowwebapp.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import javax.servlet.http.HttpSession;
+import java.util.Optional;
+
+@Controller
+public class AuthenticationController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    private static final String userSessionKey = "user";
+
+    public User getUserFromSession(HttpSession session) {
+        Integer userId = (Integer) session.getAttribute(userSessionKey);
+        if (userId == null) {
+            return null;
+        }
+
+        Optional<User> user = userRepository.findById(userId);
+
+        if (user.isEmpty()) {
+            return null;
+        }
+
+        return user.get();
+    }
+
+    private static void setUserInSession(HttpSession session, User user) {
+        session.setAttribute(userSessionKey, user.getId());
+    }
+}
