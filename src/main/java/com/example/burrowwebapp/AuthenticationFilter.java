32e252050eb662059252b61f767ada5746405cbd
John Wasielewicz <wazjohn@yahoo.com>
2021-01-15 18:07:09 -0600

    create authentication filter

---
+++ src/main/java/com/example/burrowwebapp/AuthenticationFilter.java
@@ -0,0 +1,54 @@
+package com.example.burrowwebapp;
+
+import com.example.burrowwebapp.controller.AuthenticationController;
+import com.example.burrowwebapp.data.UserRepository;
+import com.example.burrowwebapp.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+public class AuthenticationFilter extends HandlerInterceptorAdapter {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    AuthenticationController authenticationController;
+
+    private static final List<String> whitelist = Arrays.asList("/login", "/register", "/logout", "/css");
+
+    private static boolean isWhitelisted(String path) {
+        for (String pathRoot : whitelist) {
+            if (path.startsWith(pathRoot)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request,
+                             HttpServletResponse response,
+                             Object handler) throws IOException {
+
+        if (isWhitelisted(request.getRequestURI())) {
+            return true;
+        }
+
+        HttpSession session = request.getSession();
+        User user = authenticationController.getUserFromSession(session);
+
+        if (user != null) {
+            return true;
+        }
+        
+        response.sendRedirect("/login");
+        return false;
+    }
+}
