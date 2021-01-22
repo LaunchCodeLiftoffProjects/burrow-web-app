54c69baac8671edbe07b1db614b96f2f38c04569
John Wasielewicz <wazjohn@yahoo.com>
2021-01-22 08:38:34 -0600

    readd home data to models and update

--- src/main/java/com/example/burrowwebapp/controller/SearchController.java
+++ src/main/java/com/example/burrowwebapp/controller/SearchController.java
@@ -2,6 +2,7 @@ package com.example.burrowwebapp.controller;
 
 import com.example.burrowwebapp.data.DeviceRepository;
 import com.example.burrowwebapp.models.Device;
+import com.example.burrowwebapp.models.HomeData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -29,12 +30,12 @@ public class SearchController {
         Iterable<Device> devices;
         if (searchTerm.toLowerCase().equals("all") || searchTerm.equals("")){
             devices = deviceRepository.findAll();
-//        } else {
-//            devices = HomeData.findByColumnAndValue(searchType, searchTerm, deviceRepository.findAll());
+        } else {
+            devices = HomeData.findByColumnAndValue(searchType, searchTerm, deviceRepository.findAll());
         }
         model.addAttribute("columns", columnChoices);
         model.addAttribute("title", "Gophers found " + columnChoices.get(searchType) + ": " + searchTerm);
-        model.addAttribute("devices", deviceRepository.findAll());
+        model.addAttribute("devices", devices);
         model.addAttribute("result", " " + columnChoices.get(searchType));
 
         return "search";
