160e223745eb72cdf029a5fcb61440b0b84b27c7
John Wasielewicz <wazjohn@yahoo.com>
2021-01-23 11:14:27 -0600

    update fragments searchbar controller and searchbar index

--- src/main/java/com/example/burrowwebapp/controller/SearchbarController.java
+++ src/main/java/com/example/burrowwebapp/controller/SearchbarController.java
@@ -1,10 +1,15 @@
 package com.example.burrowwebapp.controller;
 
+import com.example.burrowwebapp.data.DeviceRepository;
+import com.example.burrowwebapp.models.Device;
+import com.example.burrowwebapp.models.HomeData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import static com.example.burrowwebapp.controller.ViewController.columnChoices;
 
@@ -13,11 +18,26 @@ import static com.example.burrowwebapp.controller.ViewController.columnChoices;
 public class SearchbarController {
 
     @Autowired
-    private DeviceController deviceController;
+    private DeviceRepository deviceRepository;
 
     @GetMapping
     public String search(Model model) {
         model.addAttribute("columns", columnChoices);
+        return "searchbar/index";
+    }
+
+    @PostMapping("")
+    public String displaySearchResults(Model model, @RequestParam String searchTerm){
+        Iterable<Device> devices;
+        if (searchTerm.toLowerCase().equals("all") || searchTerm.equals("")){
+            devices = deviceRepository.findAll();
+        } else {
+            devices = HomeData.findByValue(searchTerm, deviceRepository.findAll());
+        }
+        model.addAttribute("columns", columnChoices);
+        model.addAttribute("devices", devices);
+        model.addAttribute("result", ": " + searchTerm);
+
         return "searchbar/index";
     }
 }
